26ced0fed4653edf5ef27fec87e3530e88301257
ajaykumargdr <ajaykumarncas@gmail.com>
2025-01-06 21:10:13 +0530

    chore: add ai agent to give market assistance

--- simple_groq_agent.py
+++ simple_groq_agent.py
@@ -1,11 +1,17 @@
 from phi.agent import Agent
 from phi.model.groq import Groq
+from phi.tools.yfinance import YFinanceTools
 from dotenv import load_dotenv
 
 load_dotenv()
 
 agent = Agent(
-    model=Groq(id="llama-3.1-70b-versatile")
+    model=Groq(id="llama-3.1-70b-versatile"),
+    tools=[YFinanceTools(stock_price =True, analyst_recommendations=True, stock_fundamentals=True)],
+    show_tool_calls=True,
+    markdown=True,
+    instructions=["Use tables to display numbers related data."],   
 )
 
-agent.print_response("Write about the future of ai agents")
+prompt = input("Ask FinGen: ")
+agent.print_response(prompt)
